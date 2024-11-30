d625035615ee9f6704592751b5a0a095f79a272a
Arqam S A <167688229+arqamsa@users.noreply.github.com>
2024-11-30 18:19:48 +0530

    Create 16. Anagram.java

---
+++ 16. Anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char[] array1 = s1.toCharArray();
+        char[] array2 = s2.toCharArray();
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        return Arrays.equals(array1, array2);
+    }
+}
