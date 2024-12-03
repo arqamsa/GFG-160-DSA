82bf06a77b9404efcc9d371442d66091403f3dac
Arqam S A <167688229+arqamsa@users.noreply.github.com>
2024-12-03 17:52:51 +0530

    Create 19. Min Chars to Add for Palindrome.java

---
+++ 19. Min Chars to Add for Palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static int minChar(String s) {
+        // Write your code here
+        String reversed = new StringBuilder(s).reverse().toString();
+        String combined = s + "#" + reversed;
+        int[] lps = computeLPS(combined);
+        return s.length() - lps[lps.length - 1];
+    }
+    private static int[] computeLPS(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0; 
+        int i = 1;
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
