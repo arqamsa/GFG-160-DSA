1967802a4c02a2aab7264dae6f147f73c3f490aa
Arqam S A <167688229+arqamsa@users.noreply.github.com>
2024-12-12 19:14:13 +0530

    Create 28. Number of occurrence.java

---
+++ 28. Number of occurrence.java
@@ -0,0 +1,43 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int first = findFirstOccurrence(arr, target);
+        int last = findLastOccurrence(arr, target);
+        if (first == -1) {
+            return 0;
+        }
+        return last - first + 1; 
+    }
+    private int findFirstOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                high = mid - 1; 
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+    private int findLastOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                low = mid + 1; 
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+}
