5a22096b25edfa768b77025612418b2058220ad6
Arqam S A <167688229+arqamsa@users.noreply.github.com>
2025-01-18 23:24:43 +0530

    Create 65. Reverse a linked list.java

---
+++ 65. Reverse a linked list.java
@@ -0,0 +1,15 @@
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
