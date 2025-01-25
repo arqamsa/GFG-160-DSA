11419429c94723369272364c166178e0f42703d6
Arqam S A <167688229+arqamsa@users.noreply.github.com>
2025-01-25 23:01:27 +0530

    Create 72. Find the first node of loop in linked list.java

---
+++ 72. Find the first node of loop in linked list.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+        if (head == null || head.next == null) 
+            return null;
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+                break;
+        }
+        if (fast == null || fast.next == null)
+            return null;
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
